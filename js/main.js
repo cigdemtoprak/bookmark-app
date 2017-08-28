27e8c74d070c114c493e9128d05492e281b74d9f
Cigdem Toprak <cigdem.toprak@gmail.com>
2017-08-28 18:43:45 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+document.getElementById("myForm").addEventListener("submit", saveBookmark);
+
+//save bookmark
+function saveBookmark(e) {
+  var siteName = document.getElementById("siteName").value;
+  var siteUrl = document.getElementById("siteUrl").value;
+
+  if (!validateForm(siteName, siteUrl)) {
+    return false;
+  }
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  if (localStorage.getItem('bookmarks') === null) {
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  document.getElementById('myForm').reset();
+  fetchBookmarks();
+  //prevenst form from submitting
+  e.preventDefault();
+}
+
+function deleteBookmark(url) {
+  //console.log(url);
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url == url) {
+      bookmarks.splice(i, 1);
+    }
+  }
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert('Please enter the form');
+    return false;
+  }
+
+  var expression =
+    /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+  if (!siteUrl.match(regex)) {
+    alert('Please enter a valid url!');
+    return (false);
+  }
+  return true;
+}
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+  // Build output
+  bookmarksResults.innerHTML = '';
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="well">' +
+      '<h3>' + name +
+      ' <a class="btn btn-default" target="_blank" href="' + url +
+      '">Visit</a> ' +
+      ' <a onclick="deleteBookmark(\'' + url +
+      '\')" class="btn btn-danger" href="#">Delete</a> ' +
+      '</h3>' +
+      '</div>';
+  }
+}
